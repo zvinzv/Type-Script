f6f9ab3c0d36ea571e951d6771854716d5e867bd
ZVINZV | DEV <zain.business.co@gmail.com>
2023-03-31 23:29:19 +0300

    Assgniment 05 | #1-#11

--- dist/compiled/javascript/main.js
+++ dist/compiled/javascript/main.js
@@ -1,4 +1,2 @@
 "use strict";
-let arr;
-arr = [5, [true, false], ["ZVINZV", ["DEV", 16]]];
 //# sourceMappingURL=main.js.map
--- project/files/typescript/main.ts
+++ project/files/typescript/main.ts
@@ -1,7 +1,18 @@
-// let arr: (number | boolean[] | (string | (string | number)[])[])[];
-// Add any value with out error
+// function reportErrors(username, age: number) {
+//   let rank = "Professor";
+//   return `Username: ${username}`;
+//   console.log("We Will Not Reach Here");
+// }
 
-// Solve Assignment 04
+// console.log(reportErrors("Elzero", 40));
 
-let arr: (number | boolean[] | (string | (string | number)[])[])[];
-arr = [5, [true, false], ["ZVINZV", ["DEV", 16]]]
+// Solve Assignment 05
+
+/*
+[1] - tsc --init
+[2] - open tsconfig.json And Edit Below {==>"REMOVE COMMENT FROM 'tsconfig.json' TO APPLY"<==} :-
+[2.1] - "noUnusedLocals": true, => for warring about unused variable.
+[2.2] - "noUnusedParameters": true, => for warring about unused Parameters.
+[2.3] - "noImplicitAny": false, => for warring about Type Annotations.
+[2.4] - "allowUnreachableCode": false, => for warring about error reporting for unreachable code.
+*/
