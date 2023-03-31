fa7abf637cccd0b19ccf203c5077dd96f3220b1a
ZVINZV | DEV <zain.business.co@gmail.com>
2023-03-31 20:25:58 +0300

    #05 Episode

---
+++ dist/index.js
@@ -0,0 +1,20 @@
+"use strict";
+let theName = "ZVINZV";
+let TheNumber = 16;
+let hire = true;
+let all = "data";
+let all_2;
+all = 1234;
+all = "zvinzv";
+all = false;
+function add1(n1, n2) {
+    return n1 + n2;
+}
+console.log(add1(20, 30));
+console.log(typeof add1(20, 30));
+function add2(n1, n2) {
+    return n1 + n2;
+}
+console.log(add2(20, 30));
+console.log(typeof add2(20, 30));
+//# sourceMappingURL=index.js.map
