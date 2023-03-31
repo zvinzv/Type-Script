d69e764ae3c8bf8a4888b6831de0947593292ea7
ZVINZV | DEV <zain.business.co@gmail.com>
2023-03-31 20:52:30 +0300

    #06 Episode

--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,10 @@
 "use strict";
-let theName = "ZVINZV";
-let TheNumber = 16;
-let hire = true;
-let all = "data";
-let all_2;
-all = 1234;
-all = "zvinzv";
-all = false;
-function add1(n1, n2) {
-    return n1 + n2;
+let all;
+all = "ZVINZV";
+all = 112233;
+all = true;
+let myFriends = [":)", ":(", "112233"];
+for (let friend of myFriends) {
+    console.log(friend);
 }
-console.log(add1(20, 30));
-console.log(typeof add1(20, 30));
-function add2(n1, n2) {
-    return n1 + n2;
-}
-console.log(add2(20, 30));
-console.log(typeof add2(20, 30));
 //# sourceMappingURL=index.js.map
