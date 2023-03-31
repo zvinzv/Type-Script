8a1407b85abc5de624d02db571ebd35b316f481c
ZVINZV | DEV <zain.business.co@gmail.com>
2023-03-31 22:56:15 +0300

    Assignment 02 | #1-#11

--- dist/compiled/javascript/main.js
+++ dist/compiled/javascript/main.js
@@ -1,2 +1,8 @@
 "use strict";
+function calculate(numOne, numTwo) {
+    return numOne + numTwo;
+}
+console.log(calculate(10, 20));
+console.log(calculate("10", "20"));
+console.log(calculate(+true, +true));
 //# sourceMappingURL=main.js.map
---
+++ project/files/typescript/main.ts
@@ -0,0 +1,17 @@
+// function calculate(numOne, numTwo) {
+//   return numOne + numTwo;
+// }
+
+// console.log(calculate(10, 20)); // 30
+// console.log(calculate("10", "20")); // We Don't Need This To Work
+// console.log(calculate(+true, +true)); // 2
+
+// Solve The assignment 02
+
+function calculate(numOne: number, numTwo: number) {
+  return numOne + numTwo;
+}
+
+console.log(calculate(10, 20)); // 30
+console.log(calculate("10", "20")); // We Don't Need This To Work
+console.log(calculate(+true, +true)); // 2
