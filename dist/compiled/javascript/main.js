41b24f56511da4513bf56bcc07169c9f85eb99fc
ZVINZV | DEV <zain.business.co@gmail.com>
2023-03-31 23:56:33 +0300

    Assignment 08 | #1-#11

--- dist/compiled/javascript/main.js
+++ dist/compiled/javascript/main.js
@@ -1,9 +1,12 @@
 "use strict";
-function showMsg(user, age, country) {
-    return `${user}${age}${country}`;
-}
-console.log(showMsg());
-console.log(showMsg("Elzero"));
-console.log(showMsg("Elzero", 40));
-console.log(showMsg("Elzero", "40", "Egypt"));
+const printInConsole = (type, ...data) => {
+    console.log(`==================================================\nStart - ${type}\n==================================================`);
+    for (let item of data) {
+        console.log(`The Value Is '${item}' And The Type Is '${typeof item}'.`);
+    }
+    return (`==================================================\nEnd - ${type}\n==================================================\n\n`);
+};
+console.log(printInConsole("Number's", 1, 2, 3, 4, 5));
+console.log(printInConsole("Strings", "A", "B", "C"));
+console.log(printInConsole("Boolens", true, false, false, true, true));
 //# sourceMappingURL=main.js.map
--- project/files/typescript/main.ts
+++ project/files/typescript/main.ts
@@ -1,19 +1,46 @@
-// function showMsg(user: number, age: boolean, country: boolean) {
-//   return `${user}${age}${country}`;
-// }
+// // Write The Function Here
 
-// console.log(showMsg());
-// console.log(showMsg("Elzero"));
-// console.log(showMsg("Elzero", 40));
-// console.log(showMsg("Elzero", "40", "Egypt"));
+// // Using The Function => Do Not Edit
+// console.log(printInConsole(1, 2, 3, 4, 5));
+// console.log(printInConsole("A", "B", "C"));
+// console.log(printInConsole(true, false, false, true, true));
 
-// Solve Assignment 07
+// // Output
+// The Value Is 1 And Type Is number
+// The Value Is 2 And Type Is number
+// The Value Is 3 And Type Is number
+// The Value Is 4 And Type Is number
+// The Value Is 5 And Type Is number
+// Done
+// The Value Is A And Type Is string
+// The Value Is B And Type Is string
+// The Value Is C And Type Is string
+// Done
+// The Value Is true And Type Is boolean
+// The Value Is false And Type Is boolean
+// The Value Is false And Type Is boolean
+// The Value Is true And Type Is boolean
+// The Value Is true And Type Is boolean
+// Done
 
-function showMsg(user?: number | string, age?: boolean | number | string, country?: boolean | string) {
-  return `${user}${age}${country}`;
+// Solve Assignment 08
+
+
+
+
+// Write The Function Here
+
+const printInConsole = (type: string ,...data: any[]) => {
+  console.log(`==================================================\nStart - ${type}\n==================================================`)
+  for (let item of data) 
+  {
+    console.log(`The Value Is '${item}' And The Type Is '${typeof item}'.`)
+  }
+  return (`==================================================\nEnd - ${type}\n==================================================\n\n`)
 }
 
-console.log(showMsg());
-console.log(showMsg("Elzero"));
-console.log(showMsg("Elzero", 40));
-console.log(showMsg("Elzero", "40", "Egypt"));
+
+// Using The Function => Do Not Edit
+console.log(printInConsole("Number's",1, 2, 3, 4, 5));
+console.log(printInConsole("Strings","A", "B", "C"));
+console.log(printInConsole("Boolens",true, false, false, true, true));
