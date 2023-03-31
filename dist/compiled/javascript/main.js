237b520f5d80f31301ac1f1e645bd6525c156845
ZVINZV | DEV <zain.business.co@gmail.com>
2023-03-31 23:03:30 +0300

    Assignment 04 | #1-#11

--- dist/compiled/javascript/main.js
+++ dist/compiled/javascript/main.js
@@ -1,7 +1,4 @@
 "use strict";
-function printInfo(valueOne, valueTwo) {
-    return `Value One Is ${typeof valueOne == "number" ? valueOne : `"${valueOne}"`}, Value Two Is ${typeof valueTwo == "number" ? valueTwo : `"${valueTwo}"`}`;
-}
-console.log(printInfo(10, 20));
-console.log(printInfo("10", "20"));
+let arr;
+arr = [5, [true, false], ["ZVINZV", ["DEV", 16]]];
 //# sourceMappingURL=main.js.map
--- project/files/typescript/main.ts
+++ project/files/typescript/main.ts
@@ -1,19 +1,7 @@
-// function printInfo(valueOne, valueTwo) {
-//   return `Value One Is ${valueOne}, Value Two Is ${valueTwo}`;
-// }
+// let arr: (number | boolean[] | (string | (string | number)[])[])[];
+// Add any value with out error
 
-// console.log(printInfo(10, 20)); // Value One Is 10, Value Two Is 20
-// console.log(printInfo("10", "20")); // Value One Is "10", Value Two Is "20"
-// console.log(printInfo(true, [1, 2, 3])); // We Don't Need This To Work
+// Solve Assignment 04
 
-
-// Solve Assignment 03
-
-
-function printInfo(valueOne: number | string, valueTwo: number | string) {
-  return `Value One Is ${typeof valueOne == "number" ? valueOne : `"${valueOne}"`}, Value Two Is ${typeof valueTwo == "number" ? valueTwo : `"${valueTwo}"`}`;
-}
-
-console.log(printInfo(10, 20)); // Value One Is 10, Value Two Is 20
-console.log(printInfo("10", "20")); // Value One Is "10", Value Two Is "20"
-// console.log(printInfo(true, [1, 2, 3])); // We Don't Need This To Work
+let arr: (number | boolean[] | (string | (string | number)[])[])[];
+arr = [5, [true, false], ["ZVINZV", ["DEV", 16]]]
