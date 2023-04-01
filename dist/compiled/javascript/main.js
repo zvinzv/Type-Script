276b028f482e63ab61878ff5d5f472e929647487
ZVINZV | DEV <zain.business.co@gmail.com>
2023-04-01 16:15:50 +0300

    Assignment 03 | #12-#21

--- dist/compiled/javascript/main.js
+++ dist/compiled/javascript/main.js
@@ -1,5 +1,13 @@
 "use strict";
-let myInfo;
-myInfo = 1000;
-myInfo = true;
+function showInfo(data) {
+    console.log(`The Name Is ${data.theName}`);
+    console.log(`The Age Is ${data.theAge}`);
+}
+console.log(showInfo({ theName: "Elzero", theAge: 40 }));
+function showFullInfo(data) {
+    console.log(`The Name Is ${data.theName}`);
+    console.log(`The Age Is ${data.theAge}`);
+    console.log(`The Country Is ${data.country}`);
+}
+console.log(showFullInfo({ theName: "Elzero", theAge: 4, country: "Egypt" }));
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,38 @@
 // // Write Your Code Here
 
 // // Do Not Edit Here
-// let myInfo: mix;
-// myInfo = 1000; // No Problem Here
-// myInfo = true; // No Problem Here
+// function showInfo(data: Info) {
+//   console.log(`The Name Is ${data.theName}`);
+//   console.log(`The Age Is ${data.theAge}`);
+// }
+// console.log(showInfo({ theName: "Elzero", theAge: 40 }));
 
-// Solve Assignment 02
+// function showFullInfo(data: Full) {
+//   console.log(`The Name Is ${data.theName}`);
+//   console.log(`The Age Is ${data.theAge}`);
+//   console.log(`The Country Is ${data.country}`);
+// }
+// console.log(showFullInfo({ theName: "Elzero", theAge: 4, country: "Egypt" }));
+
+// Solve Assignment 03
 
 // Write Your Code Here
-type mix = number | boolean;
+type Info = {
+  theName: string;
+  theAge: number;
+  country?: string;
+};
+type Full = Info;
 // Do Not Edit Here
-let myInfo: mix;
-myInfo = 1000; // No Problem Here
-myInfo = true; // No Problem Here
+function showInfo(data: Info) {
+  console.log(`The Name Is ${data.theName}`);
+  console.log(`The Age Is ${data.theAge}`);
+}
+console.log(showInfo({ theName: "Elzero", theAge: 40 }));
+
+function showFullInfo(data: Full) {
+  console.log(`The Name Is ${data.theName}`);
+  console.log(`The Age Is ${data.theAge}`);
+  console.log(`The Country Is ${data.country}`);
+}
+console.log(showFullInfo({ theName: "Elzero", theAge: 4, country: "Egypt" }));
