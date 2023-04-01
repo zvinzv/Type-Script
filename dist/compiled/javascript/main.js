ac1498029dec386c9457bd7c9de98171e937bfc4
ZVINZV | DEV <zain.business.co@gmail.com>
2023-04-01 14:00:17 +0300

    #12 Episode

---
+++ dist/compiled/javascript/main.js
@@ -0,0 +1,7 @@
+"use strict";
+let theName = "ZVINZV";
+theName = "DEV";
+let theName2 = "ZVINZV";
+theName2 = "DEV";
+theName2 = 16;
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,16 @@
+/*
+  Data Types
+  - Type Alias
+*/
+
+type st = string;
+type stnm = string | number;
+
+let theName: st = "ZVINZV";
+theName = "DEV";
+// theName = 16;
+
+let theName2: stnm = "ZVINZV";
+theName2 = "DEV";
+theName2 = 16;
+// theName2 = true;
