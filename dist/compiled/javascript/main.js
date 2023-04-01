9f1402d89b7289af7ea1bbdd63316e6e6c9cafee
ZVINZV | DEV <zain.business.co@gmail.com>
2023-04-01 16:10:23 +0300

    Assignment 01 | #12-#21

--- dist/compiled/javascript/main.js
+++ dist/compiled/javascript/main.js
@@ -1,18 +1,5 @@
 "use strict";
-let myObject = {
-    username: "ZVINZV",
-    id: 1090,
-    skills: {
-        one: "Photoshop",
-        two: "After Effect",
-    },
-};
-myObject.id = 313;
-myObject.skills.one = "HTML";
-myObject.skills.two = "CSS";
-console.log(myObject.username);
-console.log(myObject.id);
-console.log(myObject.hire);
-console.log(myObject.skills.one);
-console.log(myObject.skills.two);
+let myData;
+myData = 1000;
+myData = +true;
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,17 @@
-/*
-  Type Annotations With Object
-*/
+// // Write Your Code Here
 
-let myObject: {
-  readonly username: string;
-  id: number;
-  hire?: boolean;
-  skills: {
-    one: string;
-    two: string;
-  };
-} = {
-  username: "ZVINZV",
-  id: 1090,
-  skills: {
-    one: "Photoshop",
-    two: "After Effect",
-  },
-};
+// // Do Not Edit Here
+// let myData: n;
+// myData = 1000; // No Problem Here
+// myData = +true; // No Problem Here
 
-// myObject.username = "DEV";
-myObject.id = 313;
-myObject.skills.one = "HTML";
-myObject.skills.two = "CSS";
+// Solve Assignment 01
 
-console.log(myObject.username);
-console.log(myObject.id);
-console.log(myObject.hire);
-console.log(myObject.skills.one);
-console.log(myObject.skills.two);
+// Write Your Code Here
+
+type n = number;
+
+// Do Not Edit Here
+let myData: n;
+myData = 1000; // No Problem Here
+myData = +true; // No Problem Here
