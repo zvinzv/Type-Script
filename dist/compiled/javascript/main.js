b13b7af0b4f26c036b46484e51cfa0ca19f0a186
ZVINZV | DEV <zain.business.co@gmail.com>
2023-04-01 15:47:01 +0300

    #19 Episode

--- dist/compiled/javascript/main.js
+++ dist/compiled/javascript/main.js
@@ -1,44 +1,4 @@
 "use strict";
-const getHardSeconds = () => {
-    return 3;
-};
-var Kids;
-(function (Kids) {
-    Kids[Kids["Five"] = 25] = "Five";
-    Kids[Kids["Seven"] = 20] = "Seven";
-    Kids[Kids["Ten"] = 15] = "Ten";
-})(Kids || (Kids = {}));
-var Level;
-(function (Level) {
-    Level[Level["Kid"] = 15] = "Kid";
-    Level[Level["Easy"] = 9] = "Easy";
-    Level[Level["Medium"] = 6] = "Medium";
-    Level[Level["Hard"] = 3] = "Hard";
-    Level[Level["Extrem"] = 2] = "Extrem";
-    Level[Level["init"] = +true] = "init";
-})(Level || (Level = {}));
-const levels = ["Kids", "Easy", "Medium", "Hard", "Extrem"];
-const newLevels = levels.map((level) => {
-    return level.toLocaleLowerCase();
-});
-const Paly = (level) => {
-    let lvl = level.toLowerCase();
-    if (newLevels.includes(lvl)) {
-        return `The Level Is ${level} And Number Of Seconds Is ${lvl === "kids"
-            ? Level.Kid
-            : lvl === "easy"
-                ? Level.Easy
-                : lvl === "medium"
-                    ? Level.Medium
-                    : lvl === "hard"
-                        ? Level.Hard
-                        : lvl === "extrem"
-                            ? Level.Extrem
-                            : undefined}`;
-    }
-    else {
-        return `Sorry That Not Found.`;
-    }
-};
-console.log(Paly("HaRd"));
+let data = "1000";
+console.log(data.repeat(2));
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,56 +1,12 @@
 /*
   Data Types
-  - Enums => Enumerations
-  --- Enum Can Refer To Other Enum
-  --- Enum Can Refer To Same Enum
-  --- Enum Can Have Calculations
-  --- Enum Can Have Functions
+  - Type Assertions
+  --- Sometime Compiler Doesnt Know The Information We Do
+  --- TypeScript Is Not Performing Any Check To Make Sure Type Assertion Is Valid
 */
+// let myImag = document.getElementById("my_img") as HTMLImageElement;
+// let myImag = <HTMLImageElement>document.getElementById("my_img");
+// console.log(myImag.src);
 
-const getHardSeconds = (): number => {
-  return 3;
-};
-
-enum Kids {
-  Five = 25,
-  Seven = 20,
-  Ten = 15,
-}
-
-enum Level {
-  Kid = Kids.Ten,
-  Easy = 9,
-  Medium = Easy - 3,
-  Hard = 3,
-  Extrem = 2,
-  init = +true,
-}
-
-const levels: string[] = ["Kids", "Easy", "Medium", "Hard", "Extrem"];
-
-const newLevels: string[] = levels.map((level) => {
-  return level.toLocaleLowerCase();
-});
-
-const Paly = (level: string): string => {
-  let lvl = level.toLowerCase();
-  if (newLevels.includes(lvl)) {
-    return `The Level Is ${level} And Number Of Seconds Is ${
-      lvl === "kids"
-        ? Level.Kid
-        : lvl === "easy"
-        ? Level.Easy
-        : lvl === "medium"
-        ? Level.Medium
-        : lvl === "hard"
-        ? Level.Hard
-        : lvl === "extrem"
-        ? Level.Extrem
-        : undefined
-    }`;
-  } else {
-    return `Sorry That Not Found.`;
-  }
-};
-
-console.log(Paly("HaRd"));
+let data: any = "1000";
+console.log((data as string).repeat(2));
