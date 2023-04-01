6d57360332c770530b5970a86e09a149ced1619f
ZVINZV | DEV <zain.business.co@gmail.com>
2023-04-01 14:22:27 +0300

    #13 Episode

--- dist/compiled/javascript/main.js
+++ dist/compiled/javascript/main.js
@@ -1,7 +1,13 @@
 "use strict";
-let theName = "ZVINZV";
-theName = "DEV";
-let theName2 = "ZVINZV";
-theName2 = "DEV";
-theName2 = 16;
+const getActions = (btns) => {
+    return [`Action For Button Up Is ${btns.up}`, `Action For Button Right Is ${btns.right}`, `Action For Button Down Is ${btns.down}`, `Action For Button Left Is ${btns.left}`, `Action For Button Boolen Is ${btns.x}`];
+};
+const [a, b, c, d, e] = getActions({
+    up: "90px",
+    right: "10px",
+    down: "40px",
+    left: "20px",
+    x: true
+});
+console.log(a, b, c, d, e);
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,28 @@
 /*
   Data Types
-  - Type Alias
+  - Advanced Type Alias
 */
 
-type st = string;
-type stnm = string | number;
+type Buttons = {
+  up: string,
+  right: string,
+  down: string,
+  left: string,
+}
 
-let theName: st = "ZVINZV";
-theName = "DEV";
-// theName = 16;
+type Last = Buttons & {
+  x: boolean
+}
+const getActions = (btns: Last) => {
+  return [`Action For Button Up Is ${btns.up}`,`Action For Button Right Is ${btns.right}`,`Action For Button Down Is ${btns.down}`,`Action For Button Left Is ${btns.left}`,`Action For Button Boolen Is ${btns.x}`] as const
+}
 
-let theName2: stnm = "ZVINZV";
-theName2 = "DEV";
-theName2 = 16;
-// theName2 = true;
+const [a,b,c,d, e] = getActions({
+  up: "90px",
+  right: "10px",
+  down: "40px",
+  left: "20px",
+  x: true
+})
+
+console.log(a,b,c,d,e)
